7dd7541ecdac386daf01d5d372ba3c817dfedc37
bradycoye <bradycoye@bradycoye.com>
2015-07-01 19:24:52 -0400

    Init
    CodeAcademy facts app

--- FunFacts/ViewController.swift
+++ FunFacts/ViewController.swift
@@ -10,8 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var funFactLabel: UILabel!
+    @IBOutlet weak var funFactButton: UILabel!
+    
+    let factBook = FactBook()
+    let colorWheel = ColorWheel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        funFactLabel.text = factBook.randomFact()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +27,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func showFunFact() {
+        var randomColor = colorWheel.randomColor()
+        funFactButton.tintColor = randomColor
+        view.backgroundColor = randomColor
+        funFactLabel.text = factBook.randomFact()
+    }
 
 }
 
